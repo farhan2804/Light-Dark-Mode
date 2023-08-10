703e1e6f4a9d7c6989d4a01c51dc3f301a4605c3
farhan <el3317.farhanmahmood@gmail.com>
2023-08-11 00:45:38 +0530

    2

--- script.js
+++ script.js
@@ -7,18 +7,16 @@ const image2 = document.getElementById('image2');
 const image3 = document.getElementById('image3');
 
 //dark mode styles
-
 function darkMode() {
     nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
     textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
     toggleIcon.children[0].textContent = 'Dark Mode';
     toggleIcon.children[1].classList.remove('fa-sun');
     toggleIcon.children[1].classList.add('fa-moon');
-    image1.src = 'img/undraw_proud_coder_light.svg';
+    image1.src = 'img/undraw_proud_coder_dark.svg';
     image2.src = 'img/undraw_feeling_proud_dark.svg';
-    image3.src = 'img/undraw_feeling_proud_light.svg';
+    image3.src = 'img/undraw_conceptual_idea_dark.svg';
 }
-
 function lightMode() {
     nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
     textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
@@ -27,7 +25,7 @@ function lightMode() {
     toggleIcon.children[1].classList.add('fa-sun');
     image1.src = 'img/undraw_proud_coder_light.svg';
     image2.src = 'img/undraw_feeling_proud_light.svg';
-    image3.src = 'img/undraw_feeling_proud_light.svg';
+    image3.src = 'img/undraw_conceptual_idea_light.svg';
 
 }
 
